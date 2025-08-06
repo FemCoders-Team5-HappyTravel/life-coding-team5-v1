3a4e5aef4dd9f8f9846ab5c42020300d5d5710e3
VitaPoperechna <vitapoperechna@gmal.com>
2025-08-06 15:15:55 +0200

    feat: added repositories

--- src/main/java/com/example/livecoding/models/Course.java
+++ src/main/java/com/example/livecoding/models/Course.java
@@ -1,30 +1,29 @@
 package com.example.livecoding.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "courses")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Course {
    @Id
-   @GeneratedValue
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
    @NotBlank
    private String name;
    private String category;
    private String description;
 
-
-
-
-
+   @OneToMany(mappedBy = "course")
+   private List<Student> students;
 }
--- src/main/java/com/example/livecoding/models/Student.java
+++ src/main/java/com/example/livecoding/models/Student.java
@@ -3,6 +3,7 @@ package com.example.livecoding.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,22 +12,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Student {
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     private String name;
+    @NotBlank
     private String surname;
     private Integer age;
 
     @ManyToOne
     @JoinColumn(name = "course_id", nullable = false)
     private Course course;
-
-
-
-
-
-
 }
